913a9fa33a951c7b03f17d70315208f19c821ef7
saltire sable <saltiresable@gmail.com>
2019-04-05 12:37:41 -0400

    Update packages

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,6 @@
 const CleanPlugin = require('clean-webpack-plugin');
 const merge = require('webpack-merge');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const path = require('path');
 
 const common = require('./webpack.common.js');
 
@@ -9,7 +8,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new CleanPlugin(['dist'], { root: path.resolve(__dirname, '..') }),
+    new CleanPlugin(),
     new OptimizeCssAssetsPlugin(),
   ],
 });
